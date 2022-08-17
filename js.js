e5b170a7dcfcfcb169a277b5fb901e93be7a9ec4
FourthStrongest <fourthstrongest@gmail.com>
2022-08-16 23:53:12 -0700

    Added Percentage Tables

--- js.js
+++ js.js
@@ -1,10 +1,27 @@
 const url = "data1_0_0.json"
 let currentDataState = null
 let sortDirection = false
+let percentTableToggle = true
 
 const tableBody = document.querySelector('.tableBody')
 const patchesDropbox = document.getElementById('patches')
+
+const currArtAcqTd = document.getElementById("currArtAcq")
+const currArtUpgTd = document.getElementById("currArtUpg")
+const currSkillAcqTd = document.getElementById("currSkillAcq")
+const currSkillUpgTd = document.getElementById("currSkillUpg")
+const artPercentTd = document.getElementById("artPercent")
+const skillPercentTd = document.getElementById("skillPercent")
+const totalPercentTd = document.getElementById("totalPercent")
+
 let patchDictionary = new Map()
+
+let currArtAcq = 0
+let currArtUpg = 0
+let maxArt = 0
+let currSkillAcq = 0
+let currSkillUpg = 0
+let maxSkill = 0
 
 window.addEventListener('DOMContentLoaded', () => {
   loadPage()
@@ -50,19 +67,25 @@ function loadData() {
 
 function loadTableData(data) {
   let dataHtml = ''
+  currArtAcq = 0
+  currArtUpg = 0
+  maxArt = 0
+  currSkillAcq = 0
+  currSkillUpg = 0
+  maxSkill = 0
 
   data.forEach(e => {
     const acquiredElement = document.createElement('td')
     const upgradedElement = document.createElement('td')
-    const acquiredHtml = `<input type='checkbox' id='acquired' onclick='onClickCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Acq`) === 'true' ? 'checked' : ''}>`
-    const upgradedHtml = `<input type='checkbox' id='upgraded' onclick='onClickCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Upg`) === 'true' ? 'checked' : ''}>`
+    const acquiredHtml = `<input type='checkbox' id='acquired' onclick='onClickAcqCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Acq`) === 'true' ? 'checked' : ''}>`
+    const upgradedHtml = `<input type='checkbox' id='upgraded' onclick='onClickUpgCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Upg`) === 'true' ? 'checked' : ''}>`
     acquiredElement.innerHTML = acquiredHtml
     upgradedElement.innerHTML = upgradedHtml
 
     let trHtml = `
     <tr id=${e.name} style='background-color: ${setBackgroundColors(acquiredElement.firstChild, upgradedElement.firstChild)}'>
-      <td>${e.type}</td>
-      <td>${getPatchedName(e.name)}</td>
+      <td id="type">${e.type}</td>
+      <td id="name">${getPatchedName(e.name)}</td>
       <td>${e.umName}</td>
       <td>${e.level}</td>
       <td>${e.region}</td>
@@ -75,6 +98,8 @@ function loadTableData(data) {
     `
 
     dataHtml += trHtml
+
+    counter(e.type, e.name)
   })
 
   tableBody.innerHTML = dataHtml
@@ -86,6 +111,35 @@ function getPatchedName(name) {
   }
 
   return name.replaceAll('_', ' ')
+}
+
+function counter(type, name) {
+  if (type === "Art") {
+    ++maxArt
+    if (localStorage.getItem(`${name}Acq`) === 'true') {
+      ++currArtAcq
+    }
+    if (localStorage.getItem(`${name}Upg`) === 'true') {
+      ++currArtUpg
+    }
+    currArtAcqTd.innerText = `${currArtAcq}/${maxArt}`
+    currArtUpgTd.innerText = `${currArtUpg}/${maxArt}`
+    artPercentTd.innerText = `${Math.round((currArtUpg / maxArt) * 100) / 100}%`
+  }
+
+  if (type === "Skill") {
+    ++maxSkill
+    if (localStorage.getItem(`${name}Acq`) === 'true') {
+      ++currSkillAcq
+    }
+    if (localStorage.getItem(`${name}Upg`) === 'true') {
+      ++currSkillUpg
+    }
+    currSkillAcqTd.innerText = `${currSkillAcq}/${maxSkill}`
+    currSkillUpgTd.innerText = `${currSkillUpg}/${maxSkill}`
+    skillPercentTd.innerText = `${Math.round((currSkillUpg / maxSkill) * 100) / 100}%`
+  }
+  totalPercentTd.innerText = `${Math.round(((currArtUpg + currSkillUpg) / (maxArt + maxSkill)) * 100) / 100}%`
 }
 
 function sortColumn(columnName) {
@@ -115,6 +169,41 @@ function sortTextColumn(columnName) {
   currentDataState = currentDataState.sort((p1, p2) => {
     return sortDirection ? (p1[columnName] > p2[columnName]) - (p1[columnName] < p2[columnName]) : (p2[columnName] > p1[columnName]) - (p2[columnName] < p1[columnName])
   })
+}
+
+function onClickAcqCheckbox(tr) {
+  const acq = tr.querySelector('#acquired')
+  const type = tr.querySelector("#type")
+  if (type.innerText === "Art") {
+    acq.checked ? ++currArtAcq : --currArtAcq
+    currArtAcqTd.innerText = `${currArtAcq}/${maxArt}`
+  }
+
+  if (type.innerText === "Skill") {
+    acq.checked ? ++currSkillAcq : --currSkillAcq
+    currSkillAcqTd.innerText = `${currSkillAcq}/${maxSkill}`
+  }
+
+  onClickCheckbox(tr)
+}
+
+function onClickUpgCheckbox(tr) {
+  const upg = tr.querySelector('#upgraded')
+  const type = tr.querySelector("#type")
+  if (type.innerText === "Art") {
+    upg.checked ? ++currArtUpg : --currArtUpg
+    currArtUpgTd.innerText = `${currArtUpg}/${maxArt}`
+    artPercentTd.innerText = `${Math.round((currArtUpg / maxArt) * 100) / 100}%`
+  }
+
+  if (type.innerText === "Skill") {
+    upg.checked ? ++currSkillUpg : --currSkillUpg
+    currSkillUpgTd.innerText = `${currSkillUpg}/${maxSkill}`
+    skillPercentTd.innerText = `${Math.round((currSkillUpg / maxSkill) * 100) / 100}%`
+  }
+  totalPercentTd.innerText = `${Math.round(((currArtUpg + currSkillUpg) / (maxArt + maxSkill)) * 100) / 100}%`
+
+  onClickCheckbox(tr)
 }
 
 function onClickCheckbox(tr) {
@@ -152,4 +241,12 @@ function onInputChange(tr) {
 function clearLocalStorage() {
   localStorage.clear()
   loadData()
+}
+
+function togglePercentTable() {
+  const percentDiv = document.getElementById("percentDiv")
+  const percentButton = document.getElementById("percentButton")
+  percentDiv.style.display = percentTableToggle ? "none" : "block"
+  percentButton.innerText = `${percentTableToggle ? "Show" : "Hide"} Percent Tables`
+  percentTableToggle = !percentTableToggle
 }
