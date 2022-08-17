14ce2ac929f2533d65dd82cbdbd737dbd6fc10ee
Rocco <roccodev.business@gmail.com>
2022-08-17 17:57:40 +0200

    Add nicer styles, add filter (#1)

--- js.js
+++ js.js
@@ -6,6 +6,9 @@ const tableBody = document.querySelector('.tableBody')
 const patchesDropbox = document.getElementById('patches')
 let patchDictionary = new Map()
 
+let showUpgraded = (localStorage.getItem("show-upgraded") || "true") === "true";
+let showAcquired = (localStorage.getItem("show-acquired") || "true") === "true";
+
 window.addEventListener('DOMContentLoaded', () => {
   loadPage()
 })
@@ -13,6 +16,10 @@ window.addEventListener('DOMContentLoaded', () => {
 function loadPage() {
   loadPatchData()
   loadData()
+  
+  // Sync checkbox status
+  document.getElementById("show-acquired").checked = showAcquired;
+  document.getElementById("show-upgraded").checked = showUpgraded;
 }
 
 function loadPatchData() {
@@ -54,21 +61,31 @@ function loadTableData(data) {
   data.forEach(e => {
     const acquiredElement = document.createElement('td')
     const upgradedElement = document.createElement('td')
-    const acquiredHtml = `<input type='checkbox' id='acquired' onclick='onClickCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Acq`) === 'true' ? 'checked' : ''}>`
-    const upgradedHtml = `<input type='checkbox' id='upgraded' onclick='onClickCheckbox(this.parentNode.parentNode)' ${localStorage.getItem(`${e.name}Upg`) === 'true' ? 'checked' : ''}>`
+    
+    const isAcquired = localStorage.getItem(`${e.name}Acq`) === 'true';
+    const isUpgraded = localStorage.getItem(`${e.name}Upg`) === 'true';
+    
+    if ((!showAcquired && isAcquired) || (!showUpgraded && isUpgraded)) {
+      return "";
+    }
+    
+    const acquiredHtml = `<input type='checkbox' class='form-check-input' id='acquired' onclick='onClickCheckbox(this.parentNode.parentNode)' ${isAcquired ? 'checked' : ''}>`
+    const upgradedHtml = `<input type='checkbox' class='form-check-input' id='upgraded' onclick='onClickCheckbox(this.parentNode.parentNode)' ${isUpgraded ? 'checked' : ''}>`
     acquiredElement.innerHTML = acquiredHtml
     upgradedElement.innerHTML = upgradedHtml
+    
+    const bgColor = setBackgroundColors(acquiredElement.firstChild, upgradedElement.firstChild);
 
     let trHtml = `
-    <tr id=${e.name} style='background-color: ${setBackgroundColors(acquiredElement.firstChild, upgradedElement.firstChild)}'>
+    <tr id=${e.name} ${bgColor ? "style='background-color: " + bgColor + "'" : ""}>
       <td>${e.type}</td>
       <td>${getPatchedName(e.name)}</td>
-      <td>${e.umName}</td>
+      <td>${e.umName.replaceAll('_', ' ')}</td>
       <td>${e.level}</td>
       <td>${e.region}</td>
       <td><img src="${e.location}"</td>
       <td>${e.upgradeGoal}</td>
-      <td><input id='prog' value=${localStorage.getItem(`${e.name}Prog`) || 0} oninput='onInputChange(this.parentNode.parentNode)'></td>
+      <td><input id='prog' class='form-control' value=${localStorage.getItem(`${e.name}Prog`) || 0} oninput='onInputChange(this.parentNode.parentNode)'></td>
       <td>${e.maxProgress}</td>
       <td>${acquiredElement.innerHTML}</td>
       <td>${upgradedElement.innerHTML}</td></tr>
@@ -105,6 +122,19 @@ function sortColumn(columnName) {
   loadTableData(currentDataState)
 }
 
+function changeFilter(checkbox, filterTypes) {
+  const {upgraded, acquired} = filterTypes;
+  if (upgraded) { // Change the upgraded filter
+    showUpgraded = checkbox.checked;
+    localStorage.setItem("show-upgraded", showUpgraded);
+  }
+  if (acquired) { // Change the acquired filter
+    showAcquired = checkbox.checked;
+    localStorage.setItem("show-acquired", showAcquired);
+  }
+  loadTableData(currentDataState);
+}
+
 function sortNumberColumn(columnName) {
   currentDataState = currentDataState.sort((e1, e2) => {
     return sortDirection ? e1[columnName] - e2[columnName] : e2[columnName] - e1[columnName]
@@ -120,9 +150,9 @@ function sortTextColumn(columnName) {
 function onClickCheckbox(tr) {
   const acq = tr.querySelector('#acquired')
   const upg = tr.querySelector('#upgraded')
-  setRowStyle(tr, acq, upg)
   localStorage.setItem(`${tr.id}Acq`, acq.checked)
   localStorage.setItem(`${tr.id}Upg`, upg.checked)
+  loadTableData(currentDataState);
 }
 
 function setRowStyle(tr, acq, upg) {
@@ -131,17 +161,16 @@ function setRowStyle(tr, acq, upg) {
 
 function setBackgroundColors(acq, upg) {
   let color = setBackgroundColor(upg)
-  if (color == 'red') color = setBackgroundColor(acq)
+  if (color == null) color = setBackgroundColor(acq)
   return color
 }
 
 function setBackgroundColor(input) {
-  const red = 'rgba(255, 0, 0, 0.5)'
-  const green = 'rgba(0, 255, 0, 0.5)'
-  const blue = 'rgba(0, 0, 255, 0.5)'
+  const green = 'rgba(118, 255, 3, 0.5)'
+  const blue = 'rgba(0, 176, 255, 0.5)'
   if (input.id == 'upgraded' && input.checked) return blue
   if (input.id == 'acquired' && input.checked) return green
-  return 'red'
+  return null
 }
 
 function onInputChange(tr) {
